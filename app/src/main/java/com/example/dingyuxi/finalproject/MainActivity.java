b474acde3999edd7f913cee44bce392c606f75a1
sophiaaading <yuxid2@illinois.edu>
2018-11-30 17:26:13 -0600

    outline

--- app/src/main/java/com/example/dingyuxi/finalproject/MainActivity.java
+++ app/src/main/java/com/example/dingyuxi/finalproject/MainActivity.java
@@ -6,12 +6,9 @@ import android.widget.Button;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ProgressBar;
+import android.content.Intent;
+
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -21,12 +18,20 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
+
+
 public class MainActivity extends AppCompatActivity {
+    /** Default logging tag for messages from the main activity. */
+    private static final String TAG = "Yelp";
+
+    /** Request queue for our network requests. */
+    private static RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        requestQueue = Volley.newRequestQueue(this);
+        setContentView(R.layout.MainActivity);
         Button myButton = (Button) findViewById(R.id.get_weather);
         myButton.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
@@ -35,12 +40,51 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        final Button startAPICall = findViewById(R.id.startAPICall);
+        Button next = (Button) findViewById(R.id.start);
+        next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), Main2Activity.class);
+                startActivityForResult(myIntent, 0);
+            }
+
+        });
+
+        final Button startAPICall = findViewById(R.id.Yelp);
         startAPICall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
                 Log.d(TAG, "Start API button clicked");
                 startAPICall();
+            }
+        });
+    }
+
+    void startAPICall() {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "",
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            Log.d(TAG, response.toString());
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.w(TAG, error.toString());
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
+
+
+
+
+
