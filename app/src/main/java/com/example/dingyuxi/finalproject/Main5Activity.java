56463dcdecd1d01a7fe84ba4145425c4f8325f88
sophiaaading <yuxid2@illinois.edu>
2018-12-11 21:52:05 -0600

    done with the project

--- app/src/main/java/com/example/dingyuxi/finalproject/Main5Activity.java
+++ app/src/main/java/com/example/dingyuxi/finalproject/Main5Activity.java
@@ -28,7 +28,6 @@ public class Main5Activity extends AppCompatActivity {
                 Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(CuisineCategory.getUrl()));
                 startActivityForResult(myIntent, 0);
                 startActivity(myIntent);
-
             }
         });
 
